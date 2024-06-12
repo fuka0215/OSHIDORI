a87b86c5d4f4d6408f5084c1cba8cc17c03afab7
fuka0215 <162338606+fuka0215@users.noreply.github.com>
2024-06-12 14:26:39 +0000

    [add]index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:new, :create, :index, :show]
   devise_for :users
   root to: "homes#top"
   get "/about" => "homes#about", as: "about"
-  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
