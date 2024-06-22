e5733272747c3bc2df99de2273f9cf36a7061245
fuka0215 <162338606+fuka0215@users.noreply.github.com>
2024-06-22 04:00:31 +0000

    [add]フラッシュメッセージ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,10 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
+      flash[:notice] = "投稿に成功しました"
       redirect_to posts_path
     else
+      flash.now[:alert] = "投稿に失敗しました"
       render :new
     end
   end
