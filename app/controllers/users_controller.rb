89646bff4a6b5b7eb57f5b68b8321f4a1530cdb2
fuka0215 <162338606+fuka0215@users.noreply.github.com>
2024-06-23 06:18:01 +0000

    [add]userのshow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
+
   def index
   end
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.post
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :posts
-  devise_for :users
+
   root to: "homes#top"
   get "/about" => "homes#about", as: "about"
+  devise_for :users
+  resources :posts
+  resources :users, only: [:index, :show, :edit]
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
