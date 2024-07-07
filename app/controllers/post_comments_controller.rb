eccec5a03f3d0616d711bed8a59c132335a60c00
fuka0215 <162338606+fuka0215@users.noreply.github.com>
2024-07-07 15:17:36 +0000

    [add]エラーメッセージの実装

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,12 +1,20 @@
 class PostCommentsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+     @post = Post.find(params[:post_id])
+     comment = current_user.post_comments.new(post_comment_params)
+     comment.post_id = @post.id
+    if comment.save
+      flash[:notice] = "送信に成功しました"
+      redirect_to post_path(@post)
+    else
+      @post_comment = PostComment.new
+      @post = Post.find(params[:post_id])
+      flash.now[:alert] = "送信に失敗しました"
+      render "posts/show"
+    end
   end
+
 
   def destroy
     PostComment.find(params[:id]).destroy
