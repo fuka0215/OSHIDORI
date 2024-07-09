9304b9508f7dfda0e81dc333000962c0a3d38084
fuka0215 <162338606+fuka0215@users.noreply.github.com>
2024-07-09 03:04:51 +0000

    [add]管理者のログイン、ログアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
 
-  root to: "homes#top"
-  get "/about" => "homes#about", as: "about"
-  devise_for :users
-  resources :posts, only: [:new, :show, :index, :edit, :create, :destroy, :update] do
-    resources :post_comments, only: [:create, :destroy]
-  end
-  resources :users, only: [:show, :index, :edit, :destroy, :update]
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
 
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
+  scope module: :public do
+    devise_for :users
+    root to: "homes#top"
+    get "/about" => "homes#about", as: "about"
+    resources :posts, only: [:new, :show, :index, :edit, :create, :destroy, :update] do
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :index, :edit, :destroy, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
