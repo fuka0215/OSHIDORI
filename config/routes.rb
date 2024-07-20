710f4b5857933ece46024946779c302c66e011c0
fuka0215 <162338606+fuka0215@users.noreply.github.com>
2024-07-20 08:37:25 +0000

    [add]フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,18 @@ Rails.application.routes.draw do
     devise_for :users
     root to: "homes#top"
     get "/about" => "homes#about", as: "about"
+
     resources :posts, only: [:new, :show, :index, :edit, :create, :destroy, :update] do
       resource :favorite, only: [:create, :destroy]
       resources :post_comments, only: [:create, :destroy]
     end
-    resources :users do
+    resources :users,only: [:show, :index, :edit, :update, :destroy]do
       member do
         get :favorites
       end
+      resource :relationships, only: [:create, :destroy]
+        get "followings" => "relationships#followings", as: "followings"
+        get "followers" => "relationships#followers", as: "followers"
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
