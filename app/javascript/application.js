bfdd0bf26fe7f84f00553f8d25159b44011fbe1f
fuka0215 <162338606+fuka0215@users.noreply.github.com>
2024-07-28 08:57:31 +0000

    [update]placesAPIを使ったオートコンプリート実装

--- app/javascript/application.js
+++
@@ -1,7 +0,0 @@
-// Entry point for the build script in your package.json
-import { Turbo } from "@hotwired/turbo-rails";
-Turbo.session.drive = false;
-import "./controllers";
-
-
-import '../place_autocomplete.js';
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,8 +12,43 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
-import '../place_autocomplete.js';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+/* global google */
+
+document.addEventListener('turbolinks:load', function () {
+  const inputAddress = document.getElementById('Address');
+  
+  if (inputAddress) {
+    // ログを出力して要素が見つかったことを確認
+    console.log('Address input element found:', inputAddress);
+
+    // オートコンプリートのオプション
+    const options = {
+      componentRestrictions: { country: 'JP' },
+    };
+
+    // オートコンプリート適用
+    const autocompleteAddress = new google.maps.places.Autocomplete(inputAddress, options);
+
+    // 住所のオートコンプリートが選択されたとき
+    autocompleteAddress.addListener('place_changed', function() {
+      const place = autocompleteAddress.getPlace();
+      
+      // 選択された場所の情報をログに出力
+      console.log('Selected place:', place);
+      
+      if (place.geometry) {
+        inputAddress.value = place.formatted_address;
+        console.log('Formatted address:', place.formatted_address);
+      } else {
+        console.log("No details available for input: '" + place.name + "'");
+      }
+    });
+  } else {
+    console.error('Address input element not found');
+  }
+});
